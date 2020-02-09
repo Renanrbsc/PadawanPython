f8746ff95f3a7b00e8449f09027de9f303f34020
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-09 17:54:59 -0300

    Definições
    Definições

---
+++ Aula01 06-11 Python/Aula1.py
@@ -0,0 +1,43 @@
+print('\nBom dia!')
+
+nome = input('Digite seu nome: ')
+print('Ola '+ nome + ', Seja bem vindo!\n')
+
+idade = 21
+print('Idade: ',idade)
+
+print('================================================================')
+soma = 5 + 8
+print('O resultado de 5 + 8 é:', soma)
+print('================================================================')
+print('\n'*2)
+
+print('Calipso mania Joelma é top')
+print(' '*5,'Calipso mania' , 'Joelma é top')
+
+print('Exalta mania')
+print('\tExalta mania')
+
+print()
+
+print('='*64, '\n'*2)
+input() # pausa a execução, continua com tecla enter
+
+
+
+# string é imutavel
+
+nome = '' # foi alocada em um endereco de memoria
+print(nome)
+nome = 'Maykon' # alteração é alocada em um novo endereço
+print(nome[2]) # string se comporta como vetor, buscando caracter da posição
+# nome[2] = 'T'  # tentativa de modificar uma posicao da string
+# print(nome) # string fica imutavel
+
+
+
+
+
+
+
+
