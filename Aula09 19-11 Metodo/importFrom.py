743c3a968e3f1142317c54ae30517c85864acede
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-09 18:01:09 -0300

    Import Biblioteca
    Import Biblioteca

---
+++ Aula09 19-11 Metodo/importFrom.py
@@ -0,0 +1,13 @@
+# Aula9 19-11-2019
+# Import From
+
+# Para importar tudo de uma biblioteca
+# from 'biblioteca'
+
+# Para importar alguns blocos de codigo da biblioteca 
+# import 'biblioteca' from 'bloco def' , 'bloco def'
+
+# Para importar todos os bloco de codigo da biblioteca 
+# import 'biblioteca' from '*'
+
+
