e121289a90314fbe745a741c2f2a3f01dbe7e700
Renan <renanrbsc@gmail.com>
2020-02-20 11:51:35 -0300

    Iniciando oo no fortwo

---
+++ Aula50_Desafio_Fortwo_OO/Resolucoes/r2/aviao.py
@@ -0,0 +1,6 @@
+from Aula50_Desafio_Fortwo_OO.Resolucoes.r2.local import Local
+
+class Aviao(Local):
+    def __init__(self):
+        pessoas = []
+        super().__init__(pessoas)
---
+++ Aula50_Desafio_Fortwo_OO/Resolucoes/r2/terminal.py
@@ -0,0 +1,11 @@
+from Aula50_Desafio_Fortwo_OO.Resolucoes.r2.local import Local
+
+class Terminal(Local):
+    def __init__(self):
+        pessoas = [
+                        'piloto', 'oficial1', 'oficial2'
+                        ,'chefe de serviço', 'comissário1', 'comissário2'
+                        ,'policial', 'presidiario'
+                    ]
+
+        super().__init__(pessoas)
