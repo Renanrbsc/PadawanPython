dc2fc2934cc1cfb64ca9d2c1ffb16dbe13bd0e86
Renan <renanrbsc@gmail.com>
2020-02-27 08:56:34 -0300

    Exercicio para converter binario e buscar o maior conjunto de zeros entre numeros 1

---
+++ Aula52 26-02 Exercicios/max_space_in_binary.py
@@ -0,0 +1,18 @@
+def convert_number_in_bynary(number):
+	binary = bin(int(number))
+	print(f"O numero convertido em binario: {binary}")
+	return binary[2:]
+
+
+def looking_for_spaces_between_binary(binary):
+	binary_number_to_split_in_list = binary.split('1')
+	list_max_number_space = []
+	for split in binary_number_to_split_in_list:
+		list_max_number_space.append(len(split))
+
+	print(f"O maior numero de espaços na lista é: {max(list_max_number_space)}")
+
+
+number = input("Informe um numero: ")
+number_binary = convert_number_in_bynary(number)
+looking_for_spaces_between_binary(number_binary)
