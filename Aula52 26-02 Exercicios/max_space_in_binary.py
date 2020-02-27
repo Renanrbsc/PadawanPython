9ecd1f8c60e100831098530195f6bed1f7a110ea
Renan <renanrbsc@gmail.com>
2020-02-27 09:10:05 -0300

    corrigindo para ignorar ultimo valor da lista

--- Aula52 26-02 Exercicios/max_space_in_binary.py
+++ Aula52 26-02 Exercicios/max_space_in_binary.py
@@ -6,6 +6,7 @@ def convert_number_in_bynary(number):
 
 def looking_for_spaces_between_binary(binary):
 	binary_number_to_split_in_list = binary.split('1')
+	binary_number_to_split_in_list.pop()
 	list_max_number_space = []
 	for split in binary_number_to_split_in_list:
 		list_max_number_space.append(len(split))
