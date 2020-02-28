c8bffcd62c7b6395c984594a19ff1fd16e2f0157
Renan <renanrbsc@gmail.com>
2020-02-28 08:25:23 -0300

    Comportamentos classe

---
+++ Aula51_21-02_Paradigmas_OO/Comportamentos_pessoa/main.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append(r'C:\Users\900159\Documents\GitHub\PadawanPython')
+
+from Aula51_21-02_Paradigmas_OO.Comportamentos_pessoa.pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+p2 = Pessoa('João', 32)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
