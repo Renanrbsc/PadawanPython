6951dfa29c51be29280e9809ea7da609652a8d14
Renan <renanrbsc@gmail.com>
2020-03-02 10:21:52 -0300

    Exercicio6 Codility TapeEquilibrium

--- Aula53 28-02 Exercicios/TapeEquilibrium/TapeEquilibrium.py
+++ Aula53 28-02 Exercicios/TapeEquilibrium/TapeEquilibrium.py
@@ -1,3 +1,22 @@
+
+# Codility test 15% Performance
+def solution(A):          
+    sum_list = sum(A)
+    list_that_ignores_the_last_value = A[:-1]
+    sum_with_next_element = A[0]
+    absolute_values_list = []
+    for index, element in enumerate(list_that_ignores_the_last_value):
+        if index != 0:
+            sum_with_next_element += A[index]
+        print(sum_with_next_element)
+        sum_list_without_element = sum_list - element
+        absolute_values_list.append(abs(sum_with_next_element - sum_list_without_element))       
+        min_value = min(absolute_values_list)
+    return min_value
+
+print(solution([3,1,2,4,3]))
+
+# Codility test 53% Performance
 def solution(A):          
     sum_list = sum(A)
     list_that_ignores_the_last_value = A[:-1]
@@ -8,5 +27,17 @@ def solution(A):
         absolute_values_list.append(abs(sum_list - 2*sum_with_next_element))       
         min_value = min(absolute_values_list)
     return min_value
+
+print(solution([3,1,2,4,3]))
+
+# Codility test 100% Performance
+def solution(A):          
+    sum_list = sum(A)
+    infinity_number = float('inf')
+    sum_with_next_element = 0
+    for i in A[:-1]:
+        sum_with_next_element += i
+        min_value_possible = min(abs(sum_list - 2*sum_with_next_element), infinity_number)
+    return min_value_possible
 
 print(solution([3,1,2,4,3]))
